1ff604e1c632d61b1c47eb8d86027a22317fbe8f
ryowuandjanet <ryowu0329@gmail.com>
2021-11-05 15:01:01 +0800

    add requirements.txt

---
+++ myprofile/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+ 
+urlpatterns = [
+	path('', views.ProfileList.as_view(), name='profile-list'),
+	path('profile/add/', views.ProfileFamilyMemberCreate.as_view(), name='profile-add'),
+	path('profile/<int:pk>', views.ProfileFamilyMemberUpdate.as_view(), name='profile-update'),
+	path('profile/<int:pk>/delete/', views.ProfileDelete.as_view(), name='profile-delete'),
+]
